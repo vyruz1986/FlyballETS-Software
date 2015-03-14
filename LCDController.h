1680154a85f4ced2e6a72687f6b061c33ee00423
vyruz1986 <alex.goris@fastlikehell.com>
2015-03-14 14:37:49 +0100

    Forgot to include header file for LCDControllerClass in previous commit.

---
+++ LCDController.h
@@ -0,0 +1,55 @@
+// LCDController.h
+
+#ifndef _LCDCONTROLLER_h
+#define _LCDCONTROLLER_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "Arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+#include <LiquidCrystal.h>
+class LCDControllerClass
+{
+ protected:
+
+
+ public:
+	void init(LiquidCrystal* Clcd1, LiquidCrystal* Clcd2);
+   void Main();
+   enum LCDFields {
+      D1Time,
+      D2Time,
+      D3Time,
+      D4Time,
+      D1CrossTime,
+      D2CrossTime,
+      D3CrossTime,
+      D4CrossTime,
+      RaceState,
+      BattLevel,
+      TeamTime,
+      TotalCrossTime
+   };
+
+private:
+   void _UpdateLCD(int _iLine, int _iPosition, String _strText, int _iFieldLength);
+   LiquidCrystal* _Clcd1;
+   LiquidCrystal* _Clcd2;
+   unsigned long _lLastLCDUpdate = 0;
+   unsigned int _iLCDUpdateInterval = 500; //500ms update interval
+
+   struct SLCDField
+   {
+      int iLine;
+      int iStartingPosition;
+      int iFieldLength;
+      String strText;
+   }_lcdfieldFields[12];
+
+};
+
+extern LCDControllerClass LCDController;
+
+#endif
+
