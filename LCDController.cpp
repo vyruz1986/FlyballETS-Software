64d91f7e3217f617dd21335772b509e53434d663
vyruz1986 <alex.goris@fastlikehell.com>
2015-03-15 17:59:56 +0100

    LCDControllerClass: Fixed dynamic usage of lcd/lcd2 object depending on which line should be changed. All 4 lines of our display are not working

--- LCDController.cpp
+++ LCDController.cpp
@@ -16,33 +16,30 @@ void LCDControllerClass::init(LiquidCrystal* Clcd1, LiquidCrystal* Clcd2)
    _Clcd2->clear();
 
    //Put initial text on screen
-   _UpdateLCD(1, 0, String("D1: 000,000s CR: 00,000s|STOP   B:    0%"), 40);
+   _UpdateLCD(1, 0, String("D1: 000,000s CR: 00,000s| STOP   B:   0%"), 40);
    _UpdateLCD(2, 0, String("D2: 000,000s CR: 00,000s| Team: 000,000s"), 40);
-   //_UpdateLCD(3, 0, String("D3: 000,000s CR: 00,000s|   CR: 000,000s"), 40);
-   //_UpdateLCD(4, 0, String("D4: 000,000s CR: 00,000s|"), 40);
+   _UpdateLCD(3, 0, String("D3: 000,000s CR: 00,000s|   CR: 000,000s"), 40);
+   _UpdateLCD(4, 0, String("D4: 000,000s CR: 00,000s|"), 40);
 
    _lcdfieldFields[D1Time] = { 1, 4, 7, String("000,000") };
    _lcdfieldFields[D2Time] = { 2, 4, 7, String("000,000") };
-   //_lcdfieldFields[D3Time] = { 3, 4, 7, String("000,000") };
-   //_lcdfieldFields[D4Time] = { 4, 4, 7, String("000,000") };
+   _lcdfieldFields[D3Time] = { 3, 4, 7, String("000,000") };
+   _lcdfieldFields[D4Time] = { 4, 4, 7, String("000,000") };
    _lcdfieldFields[D1CrossTime] = { 1, 17, 6, String("00,000") };
    _lcdfieldFields[D2CrossTime] = { 2, 17, 6, String("00,000") };
-   //_lcdfieldFields[D3CrossTime] = { 3, 17, 6, String("00,000") };
-   //_lcdfieldFields[D4CrossTime] = { 4, 17, 6, String("00,000") };
+   _lcdfieldFields[D3CrossTime] = { 3, 17, 6, String("00,000") };
+   _lcdfieldFields[D4CrossTime] = { 4, 17, 6, String("00,000") };
    _lcdfieldFields[BattLevel] = { 1, 36, 3, String("0") };
-   _lcdfieldFields[RaceState] = { 1, 25, 5, String("STOP") };
+   _lcdfieldFields[RaceState] = { 1, 25, 5, String(" STOP") };
    _lcdfieldFields[TeamTime] = { 2, 32, 7, String("000,000") };
-   
-   //_lcdfieldFields[TotalCrossTime] = { 3, 32, 7, String("000,000") };
+   _lcdfieldFields[TotalCrossTime] = { 3, 32, 7, String("000,000") };
 }
 
 void LCDControllerClass::Main()
 {
    //This is the main loop which handles LCD updates
    if ((millis() - _lLastLCDUpdate) > _iLCDUpdateInterval)
-   {  
-      //_UpdateLCD(_lcdfieldFields[RaceState].iLine, _lcdfieldFields[RaceState].iStartingPosition, _lcdfieldFields[RaceState].strText, _lcdfieldFields[RaceState].iFieldLength);
- 
+   {
       //Remark: The for statement below is a range-for loop, this is new in C++11,
       //which is not (yet) enabled by default in Arduino (as of Arduino IDE 1.6.1).
       //See this link on how to enable C++11: http://stackoverflow.com/questions/16224746/how-to-use-c11-to-program-the-arduino
@@ -63,18 +60,17 @@ void LCDControllerClass::UpdateField(LCDFields lcdfieldField, String strNewValue
 
 void LCDControllerClass::_UpdateLCD(int _iLine, int _iPosition, String _strText, int _iFieldLength)
 {
-   //TODO: dynamic switching of LCD objects doesn't work yet, so we always use _Clcd1 for now.
-   //LiquidCrystal* CActiveLCD;
-   LiquidCrystal *CActiveLCD = _Clcd1;
+   LiquidCrystal* CActiveLCD = 0;
+   //LiquidCrystal *CActiveLCD = _Clcd1;
    if (_iLine > 2)
    {
       //DisplayLine is higher than 2, this means we need to update the 2nd LCD
-      //LiquidCrystal** CActiveLCD = &_Clcd2;
+      CActiveLCD = _Clcd2;
       _iLine = _iLine - 2; //Convert line number to correct line number for 2nd display
    }
    else
    {
-      //LiquidCrystal** CActiveLCD = &_Clcd1;
+      CActiveLCD = _Clcd1;
    }
    /* Since this function is user friendly and uses diplay lines 1-4, we have to convert this number
       to a real display line (0-1) by substracting 1 again
