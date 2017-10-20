0fcaaf69b68a6964680fc573897011b73cac9395
Unknown <alex.goris@fastlikehell.com>
2017-10-21 00:01:55 +0200

    Fixed bootstrap
    - Upgraded to bootstrap v4 beta2
    - Removed bootstrap JS
    - Added ng-bootstrap
    - Fixed menu (now works with mobile devices) as per example of https://github.com/ng-bootstrap/ng-bootstrap/issues/738#issuecomment-322288752

--- FlyballETSWeb/src/app/app.module.ts
+++ FlyballETSWeb/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
+import { LoadingModule, ANIMATION_TYPES } from 'ngx-loading';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -28,7 +30,12 @@ const appRoutes: Routes = [
   imports: [
      BrowserModule,
      BrowserAnimationsModule,
-     RouterModule.forRoot(appRoutes, { useHash: true })
+     RouterModule.forRoot(appRoutes, { useHash: true }),
+     LoadingModule.forRoot({
+      animationType: ANIMATION_TYPES.rectangleBounce,
+      backdropBorderRadius: '4px'
+      }),
+      NgbModule.forRoot()
   ],
   providers: [RaceDataService],
   bootstrap: [AppComponent]
--- FlyballETSWeb/src/app/components/navbar/navbar.component.ts
+++ FlyballETSWeb/src/app/components/navbar/navbar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  navbarCollapsed = true;
   constructor() { }
 
   ngOnInit() {
