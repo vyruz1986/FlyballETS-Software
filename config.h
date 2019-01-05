9fa67e0f8495682a5ad6be896f6688f67c85554f
Unknown <alex.goris@fastlikehell.com>
2019-01-05 12:53:34 +0100

    Make version number semver compatible

--- config.h
+++ config.h
@@ -22,4 +22,4 @@
 #define WS_TICKET_BUFFER_SIZE 5           //Number of websocket tickets kept in memory
 #define WS_TIMEOUT            1800000     // Timeout for secured websocket
 
-#define APP_VER "05.00.00"
+#define APP_VER "5.0.0"
