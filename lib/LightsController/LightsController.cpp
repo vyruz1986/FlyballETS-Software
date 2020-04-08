b71aac513ed7d393e46dfd8fd5c69024fc6962a7
simonttp78 <simonttp78@gmail.com>
2020-04-08 20:50:55 +0200

    lights chain loop indentation fix 2

--- lib/LightsController/LightsController.cpp
+++ lib/LightsController/LightsController.cpp
@@ -232,7 +232,7 @@ void LightsControllerClass::ToggleLightState(Lights byLight, LightStates byLight
    }
    for(int lightschain=0; lightschain<LIGHTSCHAINS; lightschain++)
       {
-      _LightsStrip->SetPixelColor(LightConfig.iPixelNumber+5*lightschain, LightConfig.iColor);
+         _LightsStrip->SetPixelColor(LightConfig.iPixelNumber+5*lightschain, LightConfig.iColor);
       }
 
 #endif // WS281x
