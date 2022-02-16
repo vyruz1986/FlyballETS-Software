7670600520bbfd9e43e546f85de6b06f5edae5d4
simonttp78 <simonttp78@gmail.com>
2022-02-16 21:14:06 +0100

    0.9.23 Login page small tuning

--- Firmware/include/config.h
+++ Firmware/include/config.h
@@ -22,7 +22,7 @@
 #ifndef GLOBALVAR_H
 #define GLOBALVAR_H
 
-#define FW_VER "0.9.22"         // Flyball ETS firmware version
+#define FW_VER "0.9.23"         // Flyball ETS firmware version
 
 #define Simulate false          // Set to true to enable race simulation (see Simulator.h/.cpp)
 #define NumSimulatedRaces 26    // number of prepeared simulated races. Sererial interface command to change interface: e.g. "race 1"
