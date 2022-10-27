460fc7fccff350d7db05d1e8123c2d7e9a22f9b0
simonttp78 <simonttp78@gmail.com>
2022-10-27 15:23:57 +0200

    sd cards logs migration

--- src/modules/SDcardController.cpp
+++ src/modules/SDcardController.cpp
@@ -34,7 +34,7 @@ void SDcardControllerClass::init()
 
    if (!SD_MMC.begin("/SDCARD", true, false, SDMMC_FREQ_DEFAULT))
    {
-      Serial.println("Card Mount Failed");
+      log_e("Card Mount Failed");
       return;
    }
    else
@@ -42,33 +42,32 @@ void SDcardControllerClass::init()
       uint8_t cardType = SD_MMC.cardType();
       if (cardType == CARD_NONE)
       {
-         Serial.println("No SD_MMC card attached");
+         log_e("No SD_MMC card attached");
          return;
       }
-      Serial.print("\nSD_MMC Card Type: ");
       if (cardType == CARD_MMC)
       {
-         Serial.println("MMC");
+         log_i("SD_MMC Card Type: MMC");
          bSDCardDetected = true;
       }
       else if (cardType == CARD_SD)
       {
-         Serial.println("SDSC");
+         log_i("SD_MMC Card Type: SDSC");
          bSDCardDetected = true;
       }
       else if (cardType == CARD_SDHC)
       {
-         Serial.println("SDHC");
+         log_i("SD_MMC Card Type: SDHC");
          bSDCardDetected = true;
       }
       else
-         Serial.println("UNKNOWN");
+         log_i("UNKNOWN");
 
       if (bSDCardDetected)
       {
          LCDController.UpdateField(LCDController.SDcardState, "sd");
-         Serial.printf("SD_MMC Card Size: %lluMB\n\n", SD_MMC.cardSize() / (1024 * 1024));
-         Serial.printf("Free space: %lluMB\n\n", (SD_MMC.totalBytes() - SD_MMC.usedBytes()) / (1024 * 1024));
+         log_i("SD_MMC Card Size: %lluMB", SD_MMC.cardSize() / (1024 * 1024));
+         log_i("Free space: %lluMB", (SD_MMC.totalBytes() - SD_MMC.usedBytes()) / (1024 * 1024));
          if (!SD_MMC.exists("/SENSORS_DATA"))
          {
             createDir(SD_MMC, "/SENSORS_DATA");
@@ -199,13 +198,13 @@ void SDcardControllerClass::CheckSDcardSlot(uint8_t iSDdetectPin)
    _iSDdetectPinStatus = digitalRead(iSDdetectPin);
    if (bSDCardDetected && _iSDdetectPinStatus == HIGH)
    {
-      Serial.println("\nSD Card plugged out - rebooting!\n");
+      log_w("SD Card plugged out - rebooting!");
       delay(500);
       ESP.restart();
    }
    if (!bSDCardDetected && _iSDdetectPinStatus == LOW)
    {
-      Serial.println("\nSD Card plugged in - rebooting!\n");
+      log_w("SD Card plugged in - rebooting!");
       delay(500);
       ESP.restart();
    }
@@ -232,17 +231,17 @@ void SDcardControllerClass::ToggleDecimalSeparator()
 /// </summary>
 void SDcardControllerClass::listDir(fs::FS &fs, const char *dirname, uint8_t levels)
 {
-   Serial.printf("Listing directory: %s\n", dirname);
+   log_i("Listing directory: %s", dirname);
 
    File root = fs.open(dirname);
    if (!root)
    {
-      Serial.println("Failed to open directory");
+      log_e("Failed to open directory");
       return;
    }
    if (!root.isDirectory())
    {
-      Serial.println("Not a directory");
+      log_e("Not a directory");
       return;
    }
 
@@ -271,86 +270,86 @@ void SDcardControllerClass::listDir(fs::FS &fs, const char *dirname, uint8_t lev
 
 void SDcardControllerClass::createDir(fs::FS &fs, const char *path)
 {
-   Serial.printf("Creating Dir: %s\n", path);
+   log_i("Creating Dir: %s", path);
    if (fs.mkdir(path))
-      Serial.println("Dir created");
+      log_i("Dir created");
    else
-      Serial.println("mkdir failed");
+      log_e("mkdir failed");
 }
 
 void SDcardControllerClass::removeDir(fs::FS &fs, const char *path)
 {
-   Serial.printf("Removing Dir: %s\n", path);
+   log_i("Removing Dir: %s", path);
    if (fs.rmdir(path))
-      Serial.println("Dir removed");
+      log_i("Dir removed");
    else
-      Serial.println("rmdir failed");
+      log_e("rmdir failed");
 }
 
 void SDcardControllerClass::readFile(fs::FS &fs, const char *path)
 {
-   Serial.printf("Reading file: %s\n", path);
+   log_i("Reading file: %s", path);
 
    File file = fs.open(path);
    if (!file)
    {
-      Serial.println("Failed to open file for reading");
+      log_e("Failed to open file for reading");
       return;
    }
 
-   Serial.print("Read from file: ");
+   log_i("Read from file: ");
    while (file.available())
       Serial.write(file.read());
 }
 
 void SDcardControllerClass::writeFile(fs::FS &fs, const char *path, const char *message)
 {
-   Serial.printf("Writing file: %s\n", path);
+   log_i("Writing file: %s", path);
 
    File file = fs.open(path, FILE_WRITE);
    if (!file)
    {
-      Serial.println("Failed to open file for writing");
+      log_e("Failed to open file for writing");
       return;
    }
    if (file.print(message))
-      Serial.println("File written");
+      log_i("File written");
    else
-      Serial.println("Write failed");
+      log_e("Write failed");
 }
 
 void SDcardControllerClass::appendFile(fs::FS &fs, const char *path, const char *message)
 {
-   Serial.printf("Appending to file: %s\n", path);
+   log_i("Appending to file: %s", path);
 
    File file = fs.open(path, FILE_APPEND);
    if (!file)
    {
-      Serial.println("Failed to open file for appending");
+      log_e("Failed to open file for appending");
       return;
    }
    if (file.print(message))
-      Serial.println("Message appended");
+      log_i("Message appended");
    else
-      Serial.println("Append failed");
+      log_e("Append failed");
 }
 
 void SDcardControllerClass::renameFile(fs::FS &fs, const char *path1, const char *path2)
 {
-   Serial.printf("Renaming file %s to %s\n", path1, path2);
+   log_i("Renaming file %s to %s", path1, path2);
    if (fs.rename(path1, path2))
-      Serial.println("File renamed");
+      log_i("File renamed");
    else
-      Serial.println("Rename failed");
+      log_e("Rename failed");
 }
 
 void SDcardControllerClass::deleteFile(fs::FS &fs, const char *path)
 {
-   Serial.printf("Deleting file: %s\n", path);
+   log_i("Deleting file: %s", path);
    if (fs.remove(path))
-      Serial.println("File deleted");
+      log_i("File deleted");
    else
-      Serial.println("Delete failed");
+      log_e("Delete failed");
 }
 
 void SDcardControllerClass::testFileIO(fs::FS &fs, const char *path)
@@ -374,16 +373,16 @@ void SDcardControllerClass::testFileIO(fs::FS &fs, const char *path)
          len -= toRead;
       }
       end = millis() - start;
-      Serial.printf("%u bytes read for %u ms\n", flen, end);
+      log_i("%u bytes read for %u ms", flen, end);
       file.close();
    }
    else
-      Serial.println("Failed to open file for reading");
+      log_e("Failed to open file for reading");
 
    file = fs.open(path, FILE_WRITE);
    if (!file)
    {
-      Serial.println("Failed to open file for writing");
+      log_e("Failed to open file for writing");
       return;
    }
 
@@ -392,7 +391,7 @@ void SDcardControllerClass::testFileIO(fs::FS &fs, const char *path)
    for (i = 0; i < 2048; i++)
       file.write(buf, 512);
    end = millis() - start;
-   Serial.printf("%u bytes written for %u ms\n", 2048 * 512, end);
+   log_i("%u bytes written for %u ms", 2048 * 512, end);
    file.close();
 }
 
