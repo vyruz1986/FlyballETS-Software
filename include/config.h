6d48a069dce4974102f97900d4f7258a1d4c4441
simonttp78 <simonttp78@gmail.com>
2022-10-27 15:37:54 +0200

    config file update

--- include/config.h
+++ include/config.h
@@ -22,10 +22,10 @@
 #ifndef GLOBALVAR_H
 #define GLOBALVAR_H
 
-#define FW_VER "1.10.1"          // Flyball ETS firmware version
+#define FW_VER "1.10.2"          // Flyball ETS firmware version
 
 
-#define Simulate true            // Set to true to enable race simulation (see Simulator.h/.cpp)
+#define Simulate false           // Set to true to enable race simulation (see Simulator.h/.cpp)
 #define NumSimulatedRaces 48     // Number of prepeared simulated races. Sererial interface command to change interface: e.g. "race 1"
 #define TRIGGER_QUEUE_LENGTH 60  // Number of triggers in the queue
 
